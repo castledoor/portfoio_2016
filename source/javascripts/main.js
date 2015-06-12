89dad8ab0b9e5c89f615adcd65db3ff1a7c7b47b
castledoor <b.jazzy.w@gmail.com>
2015-06-12 14:01:12 -0700

    consolodates pages into one index

---
+++ source/javascripts/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $('.header-shoe').click(function() {
+    $("li").removeClass('active');
+    $('.shoe-story').addClass('active');
+  });
+
+  $('.header-hoodie').click(function() {
+    $("li").removeClass('active');
+    $('.hoodie-story').addClass('active');
+  });
+
+  $('.header-apparel').click(function() {
+    $("li").removeClass('active');
+    $('.apparel-story').addClass('active');
+  });
+});
