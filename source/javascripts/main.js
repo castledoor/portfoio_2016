58603f99935cf02b481fe06f3a6d97cc1e541859
castledoor <b.jazzy.w@gmail.com>
2015-06-22 11:05:40 -0700

    refactors some js

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -1,27 +1,40 @@
 $(document).ready(function() {
+  shoe_click_actions();
   $('.header-shoe, .bw-1').click(function() {
+    shoe_click_actions();
+  });
+
+  $('.header-hoodie, .bw-2').click(function() {
+    hoodie_click_actions();
+  });
+
+  $('.header-apparel, .bw-3').click(function() {
+    apparel_click_actions();
+  });
+
+  function shoe_click_actions() {
     $("li").removeClass('active');
     $('.shoe-story').addClass('active');
     $('.bw-1').css("opacity", "0");
     $('.bw-2').css("opacity", "1");
     $('.bw-3').css("opacity", "1");
-  });
+  }
 
-  $('.header-hoodie, .bw-2').click(function() {
+  function hoodie_click_actions() {
     $("li").removeClass('active');
     $('.hoodie-story').addClass('active');
     $('.bw-2').css("opacity", "0");
     $('.bw-1').css("opacity", "1");
     $('.bw-3').css("opacity", "1");
-  });
+  }
 
-  $('.header-apparel, .bw-3').click(function() {
+  function apparel_click_actions() {
     $("li").removeClass('active');
     $('.apparel-story').addClass('active');
     $('.bw-3').css("opacity", "0");
     $('.bw-2').css("opacity", "1");
     $('.bw-1').css("opacity", "1");
-  });
+  }
 });
 
 // DRYing
