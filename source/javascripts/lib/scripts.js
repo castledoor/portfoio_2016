23b231d8926530796950c413f19fc881f2c4acf6
castledoor <b.jazzy.w@gmail.com>
2016-02-26 11:55:30 -0800

    adds project images and carosels

--- source/javascripts/lib/scripts.js
+++ source/javascripts/lib/scripts.js
@@ -2,18 +2,35 @@ $(document).ready(function() {
   $('.work-pull').css("display", "none");
 
   $('.plus').click(function() {
+    if ($('.work-pull').hasClass("open")) {
+       var this_one = $(this).parent().parent().next()
+       var that_one = $('.plus').parent().parent().next()
+       $(that_one).not(this_one).slideUp()
+       $(that_one).not(this_one).removeClass("open").addClass("closed");
+       var b = $(that_one).parent().children().children().next().children().first();
+       var c = $(this_one).parent().children().children().next().children().first();
+       $(b).not(c).css("display", "block");
+     }
 
-    if ($(this).hasClass("closed") ){
-      document.getElementById("plus_minus").innerHTML = "-";
-    } else {
-      document.getElementById("plus_minus").innerHTML = "+";
+    if ($('.work-pull').hasClass("closed")) {
+
     }
 
+    $(this).parent().parent().next().toggleClass( "closed").toggleClass( "open");
+    $(this).parent().children().toggle();
+    $(this).parent().parent().next().slideToggle();
 
-
-
-    $(this).parent().next().slideToggle(); 
-    $(this).toggleClass( "closed", "open");
   });
+
+  $('.minus').click(function() {
+    $(this).first().parent().parent().next().slideUp().removeClass("open").addClass("closed");
+    $(this).css("display", "none");
+    $(this).prev().css("display", "block");
+  });
+
 });
 
+
+jQuery(function($) {
+$('.slider').sss();
+});
